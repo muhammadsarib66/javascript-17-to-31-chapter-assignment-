ec522245d550c7ec9a4197baf32dc51d85910335
muhammadsarib66 <65099192+muhammadsarib66@users.noreply.github.com>
2021-09-10 00:51:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,123 @@
+// CHAPTER 17 TO 31 ASSIGMENT
+
+// CH 17 TO 20 START(ARRAY & LOOP)
+
+// TASK 1 
+
+// let multidimensional = [[],[]]
+// console.log(multidimensional)
+
+// TASK 2 
+
+// let matrix = 
+// [
+// [0,1,2,3],
+// [1,0,1,2],
+// [2,1,0,1]
+// ]
+// console.log(matrix)
+
+// task 3 
+
+// for(a = 1; a<=10; a++){
+//    console.log(a)
+// }
+
+// task 4 
+
+// let = mytable = Number(prompt("enter which table u want to read"));
+// let = mytable_length = Number(prompt("length of table"))
+// if(mytable && mytable_length){
+// for (index = 1; index < mytable_length; index++ ){
+//     console.log( `${mytable}* ${index} = ${mytable*index} `)
+// }
+// }
+// else{
+//     alert('use corect number')
+// }
+
+// task 5
+
+// let fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry'];
+
+// for (a = 0; a<fruits.length; a++) {
+//   console.log (` Element at index ${a} is ${fruits[a]}`);
+// }
+
+// task 6
+
+// let counting = '';
+// let revcounting = '';
+// let even = '';
+// let oddi = '';
+// let series ='';
+
+// for (let index = 1; index < 15; index++) {
+//     counting += `${index},`
+// }
+// for (let index = 10; index >= 1; index--) {
+//     revcounting += `${index},`
+// }
+//     for (let index = 0; index <= 20; index++) {
+//         if (index % 2 === 0) {
+//           even += `${index},`;
+//         } else {  
+//           oddi += `${index},`;
+//         }
+//       }
+//       for (let index = 2; index <= 20; index++) {
+//         if (index % 2 === 0) {
+//           series += `${index}k,`;}}
+
+
+
+// console.log( `counting
+// ${counting}`);
+// console.log( `revcounting;
+// ${revcounting}`)
+// console.log(`even:
+// ${even}`)
+// console.log(`oddi:
+// ${oddi}`)
+// console.log(`series:
+// ${series}`)
+
+// task 7
+
+// let item = [`cookie`,`pastery`,`cake`]
+// let search = prompt(`Welcome to ABC bakers what you want to Order Sir/mam`)
+// let found= item.indexOf(search)
+
+// task 8
+
+// let  record =  [24, 91, 78, 53,100, 12];
+// let max = 0
+// for ( const key in record) {
+//     if(max<record[key]){
+//         max = record[key]
+//     }
+// }
+// console.log(`this is nunmbers ${record}`)
+// console.log(`this is larg number ${max}`)
+
+// task 9
+
+// let  record =  [24, 91, 78, 53,100, 12];
+// let min = record[0]
+// for ( const key in record) {
+//     if(min>record[key]){
+//         min = record[key]
+//     }
+// }
+// console.log(`this is nunmbers ${record}`)
+// console.log(`this is min number ${min}`)
+
+// task 10
+
+// let a = '';
+// for (let index = 1; index <= 20; index++) {
+//     a += `${index*5},`
+// }
+//    console.log(a)
+
+// chapter 17 to 20 end
